d2cbecf83e126d8f103bdd1cd23764ee961dbe70
TalkeenAhmadNomani <talkeennomani@gmail.com>
2025-03-20 20:50:32 +0530

    signup and login fucntion complete

--- oops_project.py
+++ oops_project.py
@@ -14,15 +14,39 @@ class chatbook:
                             4. press 4 to message your furned 
                             5. press any key to exit""")
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.login()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
+
+    def signup(self):
+        email = input("enter your email->")
+        password = input("enter your password->")
+        self.username = email
+        self.password = password
+        print("you have signuped successfully")
+        print("\n")
+        self.menu()
+    def login(self):
+        if self.username == '' and self.password== '' :
+            print("you are not logged in")
+        else:
+            user = input("pleae neter your email/username")
+            password = input("please enter your password")
+            if user == self.username and password == self.password:
+                print("you have logged in successfully")
+                self.loggedin = True
+            else:
+                print("you have entered wrong username or password")
+            print("\n")
+            self.menu()
+
+
 
 
 obj = chatbook()
